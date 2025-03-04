b19f7cb6cf4c16b28df3fc27ff25c0ef4c15985c
mdjoynalabedin61 <123541614+mdjoynalabedin61@users.noreply.github.com>
2025-03-05 00:55:27 +0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,33 +1,53 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "index.html",
-  "style.css",
-  "app.js",
-  "script12.js",
-  "about.html",
-  "backgroundimage.jpg",
-  "ramadan.png",
-  "ramadan16.png",
-  "ramadan512.png"
- // "/icons/icon-192x192.png",
-  //"/icons/icon-512x512.png"
-];
-
-// ইন্সটল ইভেন্ট
-self.addEventListener("install", event => {
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open('static')
+      .then(function (cache) {
+        cache.add('/');
+        cache.add('index.html');
+        cache.add('style.css');
+        cache.add('app.js');
+        cache.add('about.html');
+        cache.add('script.js');
+        cache.add('backgroundimage.jpg');
+        cache.add('ramadan.png');
+        cache.add('ramadan16.png');
+        cache.add('ramadan512.png');
+        cache.add('tittle-bottom.svg');
+       /* cache.add('favicon167.png');
+        cache.add('favicon180.png');
+        cache.add('favicon32.png');
+        cache.add('favicon512.png');
+        cache.add('manifest.json');
+        cache.add('sw.js');
+        cache.add('joynal.jpg');
+        cache.add('bannerbg.jpg');
+        cache.add('sohojapi-logo.png');
+        cache.add('https://kit.fontawesome.com/04a32377bf.js');
+        cache.add('https://sohojapi.vercel.app/api/divisions');
+        cache.add('https://sohojapi.vercel.app/api/districts');
+        cache.add('https://sohojapi.vercel.app/api/upzilas');
+    cache.add('https://sohojapi.vercel.app/api/unions'); */
+        cache.addAll([
+
+        ]);
+      })
   );
 });
 
-// ফ্যাচ ইভেন্ট (অফলাইনে কাজ করার জন্য)
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
   );
 });
